bc668568cec07e73fcfc60bfac5b35f5ebb193eb
Joeunjin <euji42@gmail.com>
2022-03-06 00:27:32 +0900

    배경이미지 변경, 변수 추가후 아이콘 모양 변경 추가

--- map_test/map_test.py
+++ map_test/map_test.py
@@ -4,21 +4,28 @@
 import folium
 from geopy.geocoders import Nominatim
 
+# 지도 배경 생성(이순신장군동상 기준)
+# input으로 현재 위치 받아서 설정 예정
+# zoom_start 최대 18, 최소 0
+m = folium.Map([37.5710057, 126.9747532],tiles='cartodbpositron', zoom_start=15.5)
+# m = folium.Map([37.5710057, 126.9747532],tiles='stamentoner', zoom_start=15.5)
+
 gallery_name = '서울시립미술관'
+gallery_name2 = '뮤지엄산'
 
 geolocator = Nominatim(user_agent='Museum')
 gallery = geolocator.geocode(gallery_name)
 gal_latitude = gallery.raw['lat']
 gal_longitude = gallery.raw['lon']
+# 파랑 i마크 표시
+folium.Marker(location=[gal_latitude, gal_longitude], popup=gallery_name, icon=folium.Icon(color='blue')).add_to(m)
 
-# 지도 배경 생성(이순신장군동상 기준)
-# input으로 현재 위치 받아서 설정 예정
-# zoom_start 최대 18, 최소 0
-m = folium.Map([37.5710057, 126.9747532], zoom_start=15.5)
+gallery2 = geolocator.geocode(gallery_name2)
+gal_latitude2 = gallery2.raw['lat']
+gal_longitude2 = gallery2.raw['lon']
+# 빨강 별마크 표시
+folium.Marker(location=[gal_latitude2, gal_longitude2], popup=gallery_name, icon=folium.Icon(color='red', icon='star')).add_to(m)
 
-# 마크 표시
-folium.Marker(location=[gal_latitude, gal_longitude], popup=gallery_name,
-              icon=folium.Icon(color='red')).add_to(m)
 
 # map1.html에 지도 표시
 m.save(r'map1.html')
