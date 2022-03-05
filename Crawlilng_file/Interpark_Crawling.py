838ad033a536186dff69ef4886f536dc719ff62f
DESKTOP-5E8S27K\c <cnddkscndgus@naver.com>
2022-03-05 16:48:07 +0900

    Interpark 페이지 크롤링하는 파일 업로드입니다. 현재 '일부 내용' 만 'print' 까지 가능한 상태입니다. / Interpark_Crawling.py는 Selenium 패키지 설치되고, window에서만 실행됩니다. / 자세한 Selenium 사용 방범은 https://www.notion.so/Selenium-387d89b7ff704999a0479cd0e771a16c 참고해주세요.

---
+++ Crawlilng_file/Interpark_Crawling.py
@@ -0,0 +1,30 @@
+# selenium import 부분
+import selenium
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+import time
+
+# 같은 폴더에서 크롬드라이버 불러오기
+driver = webdriver.Chrome('chromedriver_window')
+# 네이버 전시회 검색결과 띄우기
+driver.get(url='http://ticket.interpark.com/TPGoodsList.asp?Ca=Eve&SubCa=Eve_O&tid4=Eve_O')
+# 해당 페이지가 전부 로드될 때 까지 대기
+time.sleep(2)
+
+divs = driver.find_elements(by=By.CSS_SELECTOR, value='body > table > tbody > tr:nth-child(2) > td:nth-child(3) > div > div > div.con > div > table > tbody > tr')
+for div in divs:
+    a_title = div.find_element(by=By.CSS_SELECTOR, value='td.RKtxt > span > a').text
+    a_ticket_link = div.find_element(by=By.CSS_SELECTOR, value='td.RKtxt > span > a').get_attribute("href")
+    a_image_link = div.find_element(by=By.CSS_SELECTOR, value='td.RKthumb > a > img').get_attribute("src")
+    a_place = div.find_element(by=By.CSS_SELECTOR, value='td:nth-child(3) > a').text
+    a_place_link = div.find_element(by=By.CSS_SELECTOR, value='td:nth-child(3) > a').get_attribute("href")
+    a_start_date = div.find_element(by=By.CSS_SELECTOR, value='td:nth-child(4)').text
+    print(a_title, a_ticket_link, a_image_link, a_place, a_place_link, a_start_date)
+driver.close()
