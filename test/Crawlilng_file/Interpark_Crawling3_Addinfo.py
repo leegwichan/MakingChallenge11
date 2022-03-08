394f5c3136615caefd4e40c3bd35ff58104d4799
ChunganLee <cnddkscndgus@gmail.com>
2022-03-08 12:43:02 +0900

    3차 크롤링 데이터 수정 // 중복 경우 X, 코드 설명 추가

--- test/Crawlilng_file/Interpark_Crawling3_Addinfo.py
+++ test/Crawlilng_file/Interpark_Crawling3_Addinfo.py
@@ -89,7 +89,10 @@ driver.set_window_size(6000, 600)
 # 해당 페이지가 전부 로드될 때 까지 대기
 time.sleep(2)
 
+# 에외 처리 리스트 값 초기 생성  (retest_place: 추출 못한 장소 모음, overlap_list: 한 번 조사한 장소 저장 -> 다시 조사하지 않음)
 retest_place = []
+overlap_list = []
+
 
 # 수정된 데이터 다시 불러오기
 all_data = list(db.exhibition_info.find({},{'_id':False}))
@@ -100,6 +103,7 @@ for data in all_data:
 
     time.sleep(1.5)
 
+    # 제목 다듬어서 구글 지도 웹페이지에 넣고 검색
     before_address_data = data['address']
     middle_address_data = before_address_data.replace('(중계역 3번출구, 하계역 1번출구)','').replace('7층','').replace('섬유센터빌딩 B1','').replace('종로 아트 프라자 1, 2층','').replace('A동 지하','').replace('부산문화회관 전시실','').replace('G205','').replace('4층','').replace('지하 1, 2층 HYBE INSIGHT','').replace('A동 3층','').replace('지하 1층','').replace('(정동)','').replace('서울특별시 송파구 잠실로 209 (신천동) ','')
     middle_address_data2 = middle_address_data.replace('지상2층','').replace('1층','').replace('117~123층','').replace('(선흘리 2019-3)','').replace('(5,6번 게이트)','').replace('지하1','').replace('(황지동, 문화예술회관)','').replace('용인포은아트홀','').replace('브릭캠퍼스','').replace('브이센터','').replace('여수세계박람회 국제관 D블럭 3층','').replace('쎈토이박물관','').replace('진아트','').replace(' (삼성동) 무역센터 코엑스','').replace('(중계동)','')
@@ -112,38 +116,56 @@ for data in all_data:
     if not data['address'] == '온라인':
 
         time.sleep(1.5)
-        search_input_btn.click()
-        search_input_btn.send_keys(after_address_data)
 
-        time.sleep(1)
-        search_btn = driver.find_element(by=By.CSS_SELECTOR, value='#searchbox-searchbutton')
-        search_btn.click()
-        time.sleep(3)
+        # 한번 조사했을 경우에는 이미 DB에 저장되어 있으므로 DB에서 데이터를 가져와 print함
+        if data['address'] in overlap_list:
+            download_info = db.exhibition_info.find_one({'address':data['address']},{'_id':False})
+            download_latitude = download_info['latitude']
+            download_longitude = download_info['longitude']
+            print(data['title'] + '의 좌표 정보 이미 저장 되어있음.  ', download_latitude, download_longitude, '// 장소: ', after_address_data)
 
+        # 조사하지 않은 데이터는 overlap_list에 추가하고 검색 실시
+        if data['address'] not in overlap_list:
+            overlap_list = overlap_list + [data['address']]
+            search_input_btn.click()
+            search_input_btn.send_keys(after_address_data)
 
-        plus_index2 = driver.find_elements(by=By.XPATH,
-                                           value='//*[@id="pane"]/div/div[1]/div/div/div[2]/div[1]/div[3]/div/a')
-        if plus_index2:
-            a_latitude = 0
-            a_longitude = 0
-            if data['place'] not in retest_place:
-                retest_place = retest_place + [data['place']]
-            print(data['title'], '의 좌표 재조사 실시: ', a_latitude, a_longitude, '// 장소: ', data['place'])
-
-        if not plus_index2:
-            # 위도: 경도 latitude & longitude 정보 google 지도의 페이지 주소로 가져오기
-            map_url = driver.current_url
-            need_data = map_url.split('/')[6].split(',')
-            a_latitude = round(float(need_data[0].strip('@')) + 0.000062, 7)
-            a_longitude = round(float(need_data[1]) + 0.002189, 7)
-            print(data['title'],'의 좌표 정보: ',a_latitude , a_longitude, '// 장소: ', after_address_data)
+            time.sleep(1)
+            search_btn = driver.find_element(by=By.CSS_SELECTOR, value='#searchbox-searchbutton')
+            search_btn.click()
+            time.sleep(3)
 
 
+            #검색을 했을 때, 장소가 여러개가 나오는 경우에는 추후 재조사 실시, 임시적으로 0 0 을 넣어둠
+            # plus_index2는 장소가 여러개 나오는 경우 공통적인 html 부분을 나타낸 것.
 
-        db.exhibition_info.update_many({'address': data['address']}, {'$set': {'latitude': a_latitude}})
-        db.exhibition_info.update_many({'address': data['address']}, {'$set': {'longitude': a_longitude}})
+            plus_index2 = driver.find_elements(by=By.XPATH,
+                                               value='//*[@id="pane"]/div/div[1]/div/div/div[2]/div[1]/div[3]/div/a')
+            if plus_index2:
+                a_latitude = 0
+                a_longitude = 0
+                if data['place'] not in retest_place:
+                    retest_place = retest_place + [data['place']]
+                print(data['title'], '의 좌표 재조사 실시: ', a_latitude, a_longitude, '// 장소: ', data['place'])
+
+            # 검색을 했을 때, 장소가 하나로 나오는 경우, 자료를 가져옴.
+            if not plus_index2:
+                # 위도: 경도 latitude & longitude 정보 google 지도의 웹페이지 주소로 가져오기
+                map_url = driver.current_url
+                need_data = map_url.split('/')[6].split(',')
+                # 웹페이지 주소에서는 화면의 중앙의 경도,위도값이 나오기 때문에 일부 보정값을 넣어둠
+                a_latitude = round(float(need_data[0].strip('@')) + 0.000062, 7)
+                a_longitude = round(float(need_data[1]) + 0.002189, 7)
+                print(data['title'],'의 좌표 정보: ',a_latitude , a_longitude, '// 장소: ', after_address_data)
 
 
+            # 주소가 일치하는 모든 데이터에 경도,위도값을 저장
+            db.exhibition_info.update_many({'address': data['address']}, {'$set': {'latitude': a_latitude}})
+            db.exhibition_info.update_many({'address': data['address']}, {'$set': {'longitude': a_longitude}})
+
+
+
+    # 주소가 온라인인 경우에는 값을 저장하지 않음
     if data['address'] == '온라인':
         print(data['title'], '은/는 온라인으로 열림')
 
@@ -154,7 +176,7 @@ driver.close()
 
 
 
-# 예외 데이터 넣기
+# 예외 데이터 넣기 (코딩 할 수 있었으나, 머리 아파서 일일히 조사함 // 데이터가 많았다면, 제목으로 검색하는 코드를 짰을 예정)
 exception_datas = [['그라운드시소 성수',37.54639029683398, 127.06523181299563],
              ['서울시립 북서울미술관',37.64080227189487, 127.06676252475017],
              ['동탄 어둠속의대화',37.201295679171814, 127.09162539386747],
@@ -176,6 +198,7 @@ exception_datas = [['그라운드시소 성수',37.54639029683398, 127.065231812
 
 for data in exception_datas:
 
+    # 데이터 베이스에 저장 / 장소가 동일한 모든 데이터에 같은 경도,위도 값을 저장
     db.exhibition_info.update_many({'place': data[0]}, {'$set': {'latitude': data[1]}})
     db.exhibition_info.update_many({'place': data[0]}, {'$set': {'longitude': data[2]}})
     print('예외 좌표정보 추가: ' + data[0] + '//' + str(data[1]) + ', '+ str(data[2]))
